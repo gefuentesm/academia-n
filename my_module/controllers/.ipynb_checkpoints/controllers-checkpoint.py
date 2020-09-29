0c487239ceea97db55987138178ed0ea24ffd029
gefuentesm <gustavofuentesm@gmail.com>
2020-09-29 20:25:19 +0000

    fixing indents

--- my_module/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ my_module/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-
- class MyModule(http.Controller):
-     @http.route('/my_module/my_module/', auth='public')
-     def index(self, **kw):
-         return "Hello, world"
-
+class MyModule(http.Controller):
+    @http.route('/my_module/my_module/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+    
      @http.route('/my_module/my_module/objects/', auth='public')
      def list(self, **kw):
          return http.request.render('my_module.listing', {
