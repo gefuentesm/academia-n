25650aac9be9bbb16c87719e456093d006efddd2
gefuentesm <gustavofuentesm@gmail.com>
2020-10-01 16:08:02 +0000

    post test

--- my_module/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ my_module/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -24,7 +24,7 @@ class MyModule(http.Controller):
     @http.route('/webservice',type='json', methods=['POST'], auth='public')
     def find_ambassador(self,**kw):
         id =  http.request.params.get('id')
-        return json.lodad(id)['result']
+        return json.load(id)['result']
         # Testing a new route with the web server
         url = 'https://academia-n2.odoo.com'
         db = 'academia-n-principal-1361278'
