cd406fa5fcdb0a4f17b6cf3a65300e08d39f82ba
gefuentesm <gustavofuentesm@gmail.com>
2020-10-01 14:54:20 +0000

    adding post

--- my_module/controllers/controllers.py
+++ my_module/controllers/controllers.py
@@ -23,7 +23,7 @@ class MyModule(http.Controller):
     #webservice controller 
     @http.route('/webservice',type='json', methods=['POST'], auth='public')
     def find_ambassador(self,**kw):
-        return http.request.params
+        return http.request.params.result.id
         # Testing a new route with the web server
         url = 'https://academia-n2.odoo.com'
         db = 'academia-n-principal-1361278'
