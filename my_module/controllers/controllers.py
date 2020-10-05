9b0c60a118490f775d4c9afb886d511d6d6390ed
gefuentesm <gustavofuentesm@gmail.com>
2020-10-05 19:43:13 +0000

    retuning results

--- my_module/controllers/controllers.py
+++ my_module/controllers/controllers.py
@@ -24,8 +24,7 @@ class MyModule(http.Controller):
     @http.route('/webservice',type='json', methods=['POST'], auth='public')
     def find_ambassador(self,**kw):
         id =  http.request.params
-        info = json.loads(id)
-        return json.dumps(info['result'])
+        return json.dumps(id['result'])
         # Testing a new route with the web server
         url = 'https://academia-n2.odoo.com'
         db = 'academia-n-principal-1361278'
