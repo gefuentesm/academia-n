a5c165c8c1c08f2a9faf1029a3e89b57769800a6
Gustavo Fuentes <Gustavo Fuentes>
2020-10-06 02:31:24 +0000

    fixing error 500

--- my_module/controllers/controllers.py
+++ my_module/controllers/controllers.py
@@ -42,6 +42,6 @@ class MyModule(http.Controller):
         models_ids = models.execute_kw(db,uid,password,'res.partner','search'
                                        ,[[['is_company','=',True]]])
         test = models.execute_kw(db,uid,password,'res.partner', 'search_read',
-                                 [[['name','ilike',id]]
+                                 [[['name','ilike',id]],
                                  {'fields':['name','company_id']})
         #return json.dumps(test)
