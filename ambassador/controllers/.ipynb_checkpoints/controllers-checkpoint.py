d44e7e1d4848e6eeda3462dc4b76c51a19efd29e
gefuentesm <gustavofuentesm@gmail.com>
2020-10-12 16:20:35 +0000

    fixing idents

--- ambassador/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ ambassador/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+ from odoo import http
 
 
-# class Ambassador(http.Controller):
-#     @http.route('/ambassador/ambassador/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/ambassador/ambassador/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('ambassador.listing', {
-#             'root': '/ambassador/ambassador',
-#             'objects': http.request.env['ambassador.ambassador'].search([]),
-#         })
-
-#     @http.route('/ambassador/ambassador/objects/<model("ambassador.ambassador"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('ambassador.object', {
-#             'object': obj
-#         })
+class Ambassador(http.Controller):
+    @http.route('/webservice',type='json', methods=['POST'], auth='none',cors='*')
+     def find_ambassador(self,**args):
+     name = args.get('name', False)
+     # Testing a new route with the web server
+     # return name
+     url = 'https://academia-n2.odoo.com'
+     db = 'academia-n-principal-1361278'
+     username = 'harry.lopez@academia-n.com'
+     password = '12345'
+     #getting client version
+     common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+     common.version()
+     #authenticate to db
+     uid = common.authenticate(db, username, password, {})
+     # get models
+     models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+     #get all models ids
+     models_ids = models.execute_kw(db,uid,password,'res.partner','search',[[['is_company','=',True]]])
+     # query to res.partner uing ORM and fields
+     query = models.execute_kw(db,uid,password,'res.partner','search_read',[[['name','ilike',name]]],{'fields':['name','company_id']})
+    return json.dumps(query)
